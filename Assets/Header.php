09dd7cd8be13f378f63a2f6bb882c607f20a28ca
TheGamingBram <74347226+TheGamingBram@users.noreply.github.com>
2022-09-26 11:35:29 +0200

    Index + header Edit

---
+++ Assets/Header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Bon Temps</title>
+
+    <link rel="icon" type="image/png" href="https://dynamic.indigoimages.ca//v1/gifts/gifts/882709738817/1.jpg?width=614&maxheight=614">
+
+    <link rel="stylesheet" href="https://site-assets.fontawesome.com/releases/v6.2.0/css/all.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>   
+</head>
--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-
+<?php
+    include("./Assets/config.php"); //connection to database and some test functions
+    
+    
+    
+    include("./Assets/header.php"); //insert to bootstrap and other java scripts
+?>
+<h1>Hello World!</h1>
