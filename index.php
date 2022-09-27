031e005c8727e56c12183651b12c940250a6c6dc
VinkWallside <Jovey.smits@gmail.com>
2022-09-27 11:10:37 +0200

    index V3

--- index.php
+++ index.php
@@ -151,32 +151,32 @@
     <div class="container-fluid bg-light bg-icon my-5 py-6">
         <div class="container">
             <div class="section-header text-center mx-auto mb-5 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 500px;">
-                <h1 class="display-5 mb-3">Our Features</h1>
-                <p>Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed rebum vero dolor duo.</p>
+                <h1 class="display-5 mb-3">Onze menu's</h1>
+                <p>Hier vind u onze verschillende soorten menu's waar u uit kunt kiezen.</p>
             </div>
             <div class="row g-4">
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
                     <div class="bg-white text-center h-100 p-4 p-xl-5">
-                        <img class="img-fluid mb-4" src="Assets/img/icon-1.png" alt="">
-                        <h4 class="mb-3">Natural Process</h4>
-                        <p class="mb-4">Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed vero dolor duo.</p>
-                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill" href="">Read More</a>
+                        <img class="img-fluid mb-4" src="Assets/img/koe-foto.png" alt="Vlees foto">
+                        <h4 class="mb-3">Vlees</h4>
+                        <p class="mb-4">Zoals echte mannen vlees eten serveren wij ook vlees. Een lekker mals stukkie</p>
+                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill" href="">Meer info</a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
                     <div class="bg-white text-center h-100 p-4 p-xl-5">
-                        <img class="img-fluid mb-4" src="Assets/img/icon-2.png" alt="">
-                        <h4 class="mb-3">Organic Products</h4>
-                        <p class="mb-4">Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed vero dolor duo.</p>
-                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill" href="">Read More</a>
+                        <img class="img-fluid mb-4" src="Assets/img/visje.png" alt="Vis foto">
+                        <h4 class="mb-3">Vis</h4>
+                        <p class="mb-4">Ja mensen eten ook vis. Dus dan doen wij er ook maar vis bij.</p>
+                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill" href="">Meer info</a>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="bg-white text-center h-100 p-4 p-xl-5">
-                        <img class="img-fluid mb-4" src="Assets/img/icon-3.png" alt="">
-                        <h4 class="mb-3">Biologically Safe</h4>
-                        <p class="mb-4">Tempor ut dolore lorem kasd vero ipsum sit eirmod sit. Ipsum diam justo sed vero dolor duo.</p>
-                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill" href="">Read More</a>
+                        <img class="img-fluid mb-4" src="Assets/img/icon-1.png" alt="Vegan foto">
+                        <h4 class="mb-3">Vegetarisch</h4>
+                        <p class="mb-4">Ook wij serveren vegetetarisch eten. Eet gewoon vlees.</p>
+                        <a class="btn btn-outline-primary border-2 py-2 px-4 rounded-pill" href="">Meer info</a>
                     </div>
                 </div>
             </div>
