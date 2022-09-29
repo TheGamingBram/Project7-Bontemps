06c926947e3f41937674e370d8c632c5933cdb8a
VinkWallside <Jovey.smits@gmail.com>
2022-09-29 13:38:24 +0200

    .

--- index.php
+++ index.php
@@ -166,8 +166,6 @@
 
 
     <!-- JavaScript Libraries -->
-    <!-- <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script> -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script> -->
     <script src="Assets/lib/wow/wow.min.js"></script>
     <script src="Assets/lib/easing/easing.min.js"></script>
     <script src="Assets/lib/waypoints/waypoints.min.js"></script>
