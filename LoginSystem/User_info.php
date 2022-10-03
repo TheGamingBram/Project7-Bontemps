a1aa6b2f049bc711106f860d40d80a73823a2351
TheGamingBram <74347226+TheGamingBram@users.noreply.github.com>
2022-10-03 12:13:54 +0200

    Login / Regist Update

---
+++ LoginSystem/User_info.php
@@ -0,0 +1,21 @@
+<?php 
+    include("../Assets/config.php"); //connection to database and some test functions
+    include("../Assets/header.php"); //insert to bootstrap and other java scripts
+
+    session_start();
+
+    prettyprint($_SESSION);
+    if($_SESSION["login"] == "true" && isset($_SESSION['userid'])){
+        $sql = "SELECT Naam, Email, Telefoonnummer, Adres, Postcode, Plaats FROM klanten WHERE ID = ?"
+        if($stmt = mysqli_prepare($link, $sql)){{
+            mysqli_stmt_bind_param($stmt, "s", $param_id);
+            $param_id = $_SESSION['userid'];
+            if(mysqli_stmt_execute($stmt)){
+                mysqli_stmt_store_result($stmt);
+                mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
+            }
+        }
+    }else {
+        header("location: index.php");
+    }
+?>
