2fba873734c21692977101d345d36c2dcaf7502d
XanderGeurts <xander.geurts@rocdeleijgraaf.nl>
2022-10-03 11:59:29 +0200

    booking changes

--- bookingen/index.php
+++ bookingen/index.php
@@ -8,6 +8,37 @@
     // include('../header.php');
     include('../Assets/Checklogin.php');
 
+    $_SESSION['userid'] = "5";
+
+    $KlantID = $_SESSION['userid'];
+
+    $QueryBooking = "SELECT Datum, Aantal, Klanten_ID AS KlantenID, Tafel_ID AS TafelID FROM reserveringen WHERE Klanten_ID = '$KlantID'";
+    $resultBooking=$con->query($QueryBooking);
+    if($stmt = mysqli_prepare($con, $QueryBooking)){
+       while($rowBooking = $resultBooking->fetch_assoc())
+       {
+        if($rowBooking != NULL)
+        {   
+            $Datum = $rowBooking['Datum'];
+            $Aantal = $rowBooking['Aantal'];
+            $KlantID = $rowBooking['KlantenID'];
+            $TafelID = $rowBooking['TafelID'];
+        }
+       }
+    }
+
+    $Queryklanten = "SELECT Naam FROM klanten WHERE ID = '$KlantID'";
+    $resultklanten=$con->query($Queryklanten);
+    if($stmt = mysqli_prepare($con, $Queryklanten)){
+       while($rowklanten = $resultklanten->fetch_assoc())
+       {
+        if($rowklanten != NULL)
+        {
+            $KlantNaam = $rowklanten['Naam'];
+        }
+       }
+    }
+
 ?>
 
 
