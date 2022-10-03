b357ab5a4d56e3695b90583213084003cbd6d648
XanderGeurts <xander.geurts@rocdeleijgraaf.nl>
2022-10-03 12:16:18 +0200

    changes view booking

--- bookingen/index.php
+++ bookingen/index.php
@@ -15,6 +15,7 @@
     $QueryBooking = "SELECT Datum, Aantal, Klanten_ID AS KlantenID, Tafel_ID AS TafelID FROM reserveringen WHERE Klanten_ID = '$KlantID'";
     $resultBooking=$con->query($QueryBooking);
     if($stmt = mysqli_prepare($con, $QueryBooking)){
+        $varBooking = "";
        while($rowBooking = $resultBooking->fetch_assoc())
        {
         if($rowBooking != NULL)
@@ -23,52 +24,75 @@
             $Aantal = $rowBooking['Aantal'];
             $KlantID = $rowBooking['KlantenID'];
             $TafelID = $rowBooking['TafelID'];
-        }
-       }
-    }
 
-    $Queryklanten = "SELECT Naam FROM klanten WHERE ID = '$KlantID'";
-    $resultklanten=$con->query($Queryklanten);
-    if($stmt = mysqli_prepare($con, $Queryklanten)){
-       while($rowklanten = $resultklanten->fetch_assoc())
-       {
-        if($rowklanten != NULL)
-        {
-            $KlantNaam = $rowklanten['Naam'];
+            $Queryklanten = "SELECT Naam FROM klanten WHERE ID = '$KlantID'";
+            $resultklanten=$con->query($Queryklanten);
+            if($stmt = mysqli_prepare($con, $Queryklanten)){
+               while($rowklanten = $resultklanten->fetch_assoc())
+               {
+                if($rowklanten != NULL)
+                {
+                    $KlantNaam = $rowklanten['Naam'];
+                }
+               }
+            }
+
+            $varBooking .= "<tr> <td>$Aantal</td> <td>$KlantNaam</td> <td>$TafelID</td> <td>$Datum</td> </tr>";
+
+
         }
        }
     }
 
 ?>
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
 <body>
 
     <div class="container">
-        <table id="bookingenTable" class="display">
-            <thead>
-                <tr>
-                    <th>Aantal persoonen</th>
-                    <th>Gast</th>
-                    <th>Tafel</th>
-                    <th>Datum</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Row 1 Data 1</td>
-                    <td>Row 1 Data 2</td>
-                    <td>Row 1 Data 1</td>
-                    <td>Row 1 Data 2</td>
-                </tr>
-                <tr>
-                    <td>Row 2 Data 1</td>
-                    <td>Row 2 Data 2</td>
-                    <td>Row 1 Data 1</td>
-                    <td>Row 1 Data 2</td>
-                </tr>
-            </tbody>
-        </table>
+        <div class="card my-4">
+            <div class="card-header">
+                 <div class="row justify-content-between">
+                        <div class="col-auto">
+                            <div class="card-title">
+                                <h1>Booking overzicht</h1>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="row">
+                                <div class="col-auto">
+                                    <a href="booking.php" class="btn btn-success"> 
+                                    Booking plaatsen
+                                    </a>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
+            </div>
+            <div class="card-body">
+                <table id="bookingenTable" class="display">
+                    <thead>
+                        <tr>
+                            <th>Aantal persoonen</th>
+                            <th>Gast</th>
+                            <th>Tafel</th>
+                            <th>Datum</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php 
+                            echo $varBooking;
+                        ?>
+                    </tbody>
+                </table>
+            </div> 
+        </div> 
     </div>
 </body>
 
