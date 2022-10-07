0212b823bbd94da8253f33602b030d8ff20e7f7f
TheGamingBram <74347226+TheGamingBram@users.noreply.github.com>
2022-10-07 13:42:01 +0200

    Login System

--- Assets/Header.php
+++ Assets/Header.php
@@ -43,8 +43,17 @@
                         <div class="fa fa-user text-body"></div>
                     </a>
                     <div class="dropdown-menu m-0">
-                        <button data-bs-target="#exampleModalToggle" data-bs-toggle="modal" class="dropdown-item">Inloggen</button>
-                        <a class="dropdown-item">Registeren</a>
+                        <?php 
+                        
+                        if(!isset($_SESSION['login'])){
+                            echo "
+                            <button data-bs-target='#exampleModalToggle' data-bs-toggle='modal' class='dropdown-item'>Inloggen</button>
+                            <button data-bs-target='#exampleModalToggle2' data-bs-toggle='modal' class='dropdown-item'>Registreren</button>
+                            ";
+                        }
+
+                        ?>
+                        
                     </div>
                 </div>
             </div>
@@ -81,6 +90,62 @@
       </div>
     </div>
 </div>
+<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalToggleLabel2">Registreer</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form class="form-horizontal" method=post action="" autocomplete="off">
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="Name-name" class="col-form-label">Naam:</label>
+                    <input type="text" required name="name" class="form-control" />
+                </div>
+                <div class="mb-3">
+                    <label for="email-name" class="col-form-label">Email:</label>
+                    <input type="email" required name="Email" class="form-control" />
+                </div>
+                <div class="mb-3">
+                    <label for="email-name" class="col-form-label">Password:</label>
+                    <input type="password" required name="Password" class="form-control" />
+                </div>
+                <div class="mb-3">
+                    <label for="tel-name" class="col-form-label">Telefoonnummer:</label>
+                    <input type="tel" required name="phone" pattern="[0-9]{10}" class="form-control" />
+                </div>
+                <div class="row justify-content-center align-items-center g-2">
+                    <div class="col">
+                        <div class="mb-3">
+                            <label for="email-name" class="col-form-label">Adress:</label>
+                            <input type="text" class="form-control" required name="Adress"/>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="mb-3">
+                            <label for="email-name" class="col-form-label">Postcode:</label>
+                            <input type="text" class="form-control" required name="Postcode" pattern="[0-9]{4}[A-Z]{2}"/>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="mb-3">
+                            <label for="email-name" class="col-form-label">Plaats:</label>
+                            <input type="text" class="form-control" required name="Plaats"/>
+                        </div>
+                    </div>
+                </div>
+                <input type="hidden" value="Regist" name="Type">
+            </div>
+            <div class="modal-footer">
+                <button type="submit" value="Submit" name="submit" class="btn btn-primary">Submit</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </form>
+      </div>
+    </div>
+</div>
+
 <!-- Inlog & Registreer Modal END -->
 
 <!-- Spacer -->
